45d4f05abff91db4a2ab713387e29f1d2679d4fc
zhanggehui <395510548@qq.com>
2020-11-06 18:54:24 +0800

    update

--- lmp.sh
+++ lmp.sh
@@ -13,7 +13,8 @@
 hosts=`scontrol show hostname $SLURM_JOB_NODELIST`; echo $hosts
 
 source /appsnew/source/intel2018.sh
-export PATH=/appsnew/physapps/lammps/lammps-7Aug192/bin3/bin/:$PATH #good
+export PATH=/appsnew/physapps/lammps/lammps-7Aug192/bin/bin/:$PATH  # 1,2
+#export PATH=/appsnew/physapps/lammps/lammps-7Aug19/bin/bin/:$PATH  # 1,3
 
 #source /appsnew/source/lammps-7Aug19-intel2018mpi-add.sh
 #export PATH=/appsnew/physapps/lammps/lammps-7Aug192/bin2/bin:$PATH
