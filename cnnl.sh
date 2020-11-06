f626c5e303472431a0364c26b09bd70c171b8597
zhanggehui <395510548@qq.com>
2020-11-06 15:35:30 +0800

    first commit

---
+++ cnnl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -J lmp_%j
+#SBATCH -p cn_nl
+#SBATCH -N 3
+#SBATCH -o 1.out
+#SBATCH -e 2.err
+#SBATCH --no-requeue
+#SBATCH -A liufeng_g1
+#SBATCH --qos=liufengcnnl
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+
+hosts=`scontrol show hostname $SLURM_JOB_NODELIST`; echo $hosts
+source /appsnew/source/lammps-7Aug19-intel2018mpi-add.sh
+mpirun -np $SLURM_NTASKS lmp -in in.CHO
